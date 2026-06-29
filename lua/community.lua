b0f0c5b1945245b92c6330a82757cda6a3a5101a
sud0t <klgrway@gmail.com>
2026-06-29 06:02:15 +0000

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,18 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Python
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.basedpyright" },
+  { import = "astrocommunity.pack.python.black" },
+  { import = "astrocommunity.pack.python.isort" },
+  -- Lua
   { import = "astrocommunity.pack.lua" },
+  -- Biome
+  { import = "astrocommunity.pack.biome" },
+  -- Bash
+  { import = "astrocommunity.pack.bash" },
+  -- nvim dap view because I prefer a more minimal graphical debugger
+  { import = "astrocommunity.nvim-dap-view" },
   -- import/override with your plugins folder
 }
