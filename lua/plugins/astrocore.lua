0dfd78f4cf171137dd3c9539847b9fcec457aba5
sud0t <klgrway@gmail.com>
2026-06-29 04:01:06 +0000

    Update astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -40,7 +40,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
+        relativenumber = false, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
@@ -58,10 +58,6 @@ return {
       -- first key is the mode
       n = {
         -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
